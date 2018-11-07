978f5e5fb4fc816a47bcb8a1914866437c220ef4
Roman Mashenkin <xromash@vxdesign.store>
2018-11-07 21:32:46 +0300

    Initial Commit

---
+++ Phone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Phone
+//
+//  Created by Роман Машенькин on 07/11/2018.
+//  Copyright © 2018 vXdesign.store. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
