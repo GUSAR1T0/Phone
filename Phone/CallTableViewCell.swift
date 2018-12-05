31fa1dbec8885e9fba1a8d88311cde5a025aafd9
Roman Mashenkin <xromash@vxdesign.store>
2018-12-05 09:30:49 +0300

    Add new tabs
    Signed-off-by: Roman Mashenkin <xromash@vxdesign.store>

---
+++ Phone/CallTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CallTableViewCell.swift
+//  Phone
+//
+//  Created by Роман Машенькин on 04/12/2018.
+//  Copyright © 2018 vXdesign.store. All rights reserved.
+//
+
+import UIKit
+
+class CallTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
